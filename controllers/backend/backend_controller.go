d6d23e15e9b6ff5795a0e71f8d6e7bc9513fd3ef
Carlos de Paula <me@carlosedp.com>
2020-09-05 12:48:38 -0300

    More groundwork for the backend interface

--- controllers/backend/backend_controller.go
+++ controllers/backend/backend_controller.go
@@ -1,19 +1,32 @@
 package backend
 
-type BackendProvider interface {
+import "context"
+
+// Provider interface method signatures
+type Provider interface {
 	Connect() error
 	GetMonitor()
 	GetPool()
 	GetVIP()
-	CreateMonitor() (string, error)
-	CreatePool() (string, error)
-	CreateVIP() (string, error)
+
+	CreateMonitor(ctx context.Context, name string, url string, port int) (string, error)
+	EditMonitor(ctx context.Context, name string, url string, port int) (string, error)
+	DeleteMonitor(ctx context.Context, name string, url string, port int) (string, error)
+
+	CreateMember(ctx context.Context, node string, IP string) (string, error)
+	EditPoolMember(ctx context.Context, name string, member string, port int, status string) (string, error)
+	DeletePoolMember(ctx context.Context, name string, member string, port int, status string) (string, error)
+
+	CreatePool(ctx context.Context, name string, monitor string, members []string, port int) (string, error)
+	EditPool(ctx context.Context, name string, monitor string, members []string, port int) (string, error)
+	DeletePool(ctx context.Context, name string, monitor string, members []string, port int) (string, error)
+
+	CreateVIP(ctx context.Context, name string, VIP string, pool string, port int) (string, error)
+	EditVIP(ctx context.Context, name string, VIP string, pool string, port int) (string, error)
+	DeleteVIP(ctx context.Context, name string, VIP string, pool string, port int) (string, error)
 }
 
-type Provider struct {
-	providerVendor string
-}
-
+// ProviderOptions contains the connection parameters for the Provider
 type ProviderOptions struct {
 	host          string
 	hostport      int
@@ -25,15 +38,47 @@ type ProviderOptions struct {
 
 //HandleMonitors manages the Monitor validation, update and creation
 func HandleMonitors() error {
+	// Check if monitor exists
+
+	// Create Monitor
+
+	// or Update Monitor ports and parameters
 	return nil
 }
 
 //HandlePool manages the Pool validation, update and creation
-func HandlePool(nodeIPs map[string]string) error {
+func HandlePool(name string, nodeIPs map[string]string, port int) error {
+	// Check if pool exists
+
+	// if doesn't exist, create pool
+
+	// Check pool members
+
+	// Create pool members that do not exist
+
+	// update pool adding new members and removing not used ones
+
 	return nil
 }
 
 //HandleVIP manages the VIP validation, update and creation
 func HandleVIP() error {
+	// Check if VIP exists
+
+	// if doesn't exist, create VIP
+
+	// attach pool
+
+	// update VIP ports and parameters
+
+	return nil
+}
+
+func createMember(members map[string]string) error {
+	for k, v := range members {
+		nodeName := k
+		IP := v
+		_, _ = nodeName, IP
+	}
 	return nil
 }
--- controllers/backend/f5_controller.go
+++ controllers/backend/f5_controller.go
@@ -24,7 +24,7 @@ type F5Provider struct {
 	validatecerts bool
 }
 
-// Connect creates a connection to the F5 Big IP Load Balancer
+// Connect creates a connection to the IP Load Balancer
 func (b F5Backend) Connect() error {
 	host := b.provider.host + ":" + strconv.Itoa(b.provider.hostport)
 	b.f5 = bigip.NewSession(host, b.provider.username, b.provider.password, nil)
@@ -32,7 +32,27 @@ func (b F5Backend) Connect() error {
 	return nil
 }
 
-// CreateMonitor creates a monitor in the F5 Big IP Load Balancer
+// GetMonitor gets a monitor in the IP Load Balancer
+func (b F5Backend) GetMonitor(ctx context.Context, name string) (string, error) {
+	return "", nil
+}
+
+// GetPool gets a server pool in the IP Load Balancer
+func (b F5Backend) GetPool(ctx context.Context, name string) (string, error) {
+	_, err := b.f5.GetPool(name)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	return name, nil
+}
+
+// GetVIP gets a VIP in the IP Load Balancer
+func (b F5Backend) GetVIP(ctx context.Context, name string) (string, error) {
+	return "", nil
+}
+
+// CreateMonitor creates a monitor in the IP Load Balancer
 // if port argument is 0, no port override is configured
 func (b F5Backend) CreateMonitor(ctx context.Context, name string, url string, port int) (string, error) {
 
@@ -58,18 +78,107 @@ func (b F5Backend) CreateMonitor(ctx context.Context, name string, url string, p
 	return name, nil
 }
 
-// CreatePool creates a server pool in the F5 Big IP Load Balancer
-func (b F5Backend) CreatePool(ctx context.Context, name string) (string, error) {
-	_, err := b.f5.GetPool(name)
+// EditMonitor creates a monitor in the IP Load Balancer
+// if port argument is 0, no port override is configured
+func (b F5Backend) EditMonitor(ctx context.Context, name string, url string, port int) (string, error) {
+	return name, nil
+}
+
+// DeleteMonitor creates a monitor in the IP Load Balancer
+// if port argument is 0, no port override is configured
+func (b F5Backend) DeleteMonitor(ctx context.Context, name string, url string, port int) (string, error) {
+	return name, nil
+}
+
+// CreateMember creates a member to be added to pool in the Load Balancer
+func (b F5Backend) CreateMember(ctx context.Context, node string, IP string) (string, error) {
+	err := b.f5.CreateNode(node, IP)
 	if err != nil {
 		fmt.Println(err)
 		return "", err
 	}
-	b.f5.AddMonitorToPool("web_http", "web_80_pool")
+	return node, nil
+}
+
+// EditPoolMember modifies a server pool member in the Load Balancer
+// status could be "enable" or "disable"
+func (b F5Backend) EditPoolMember(ctx context.Context, name string, member string, port int, status string) (string, error) {
+	err := b.f5.PoolMemberStatus(name, member+":"+strconv.Itoa(port), status)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
 	return name, nil
 }
 
-// CreateVIP creates a Virtual Server in the F5 Big IP Load Balancer
-func (b F5Backend) CreateVIP(ctx context.Context, name string) (string, error) {
+// DeletePoolMember deletes a member in the Load Balancer
+func (b F5Backend) DeletePoolMember(ctx context.Context, name string, member string, port int, status string) (string, error) {
+	// First delete member from pool
+	err := b.f5.DeletePoolMember(name, member+":"+strconv.Itoa(port))
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	// Then delete node (TEST)
+	err = b.f5.DeleteNode(member)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	return name, nil
+}
+
+// CreatePool creates a server pool in the Load Balancer
+func (b F5Backend) CreatePool(ctx context.Context, name string, monitor string, members []string, port int) (string, error) {
+
+	// Create Pool
+	err := b.f5.CreatePool(name)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	// Add members to Pool
+	for _, m := range members {
+		b.f5.AddPoolMember(name, m+":"+strconv.Itoa(port))
+	}
+
+	// Add monitor to Pool
+	err = b.f5.AddMonitorToPool(monitor, name)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	return name, nil
+}
+
+// EditPool modifies a server pool in the Load Balancer
+func (b F5Backend) EditPool(ctx context.Context, name string, monitor string, members []string, port int) (string, error) {
+	return name, nil
+}
+
+// DeletePool removes a server pool in the Load Balancer
+func (b F5Backend) DeletePool(ctx context.Context, name string, monitor string, members []string, port int) (string, error) {
+	return name, nil
+}
+
+// CreateVIP creates a Virtual Server in the Load Balancer
+func (b F5Backend) CreateVIP(ctx context.Context, name string, VIP string, pool string, port int) (string, error) {
+	// The second parameter is our destination, and the third is the mask. You can use CIDR notation if you wish (as shown here)
+	err := b.f5.CreateVirtualServer(name, VIP, "0", pool, port)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	return name, nil
+}
+
+// EditVIP modifies a Virtual Server in the Load Balancer
+func (b F5Backend) EditVIP(ctx context.Context, name string, VIP string, pool string, port int) (string, error) {
+	return name, nil
+}
+
+// DeleteVIP deletes a Virtual Server in the Load Balancer
+func (b F5Backend) DeleteVIP(ctx context.Context, name string, VIP string, pool string, port int) (string, error) {
 	return name, nil
 }
