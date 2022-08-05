f5209569680d7be0971976f8e6479823c01c694b
Carlos de Paula <me@carlosedp.com>
2022-08-05 12:11:55 -0300

    Fix handling netscaler connection. Adjust some sample yaml

--- controllers/backend/netscaler/netscaler_controller.go
+++ controllers/backend/netscaler/netscaler_controller.go
@@ -76,7 +76,7 @@ func (p *NetscalerProvider) Create(ctx context.Context, lbBackend lbv1.Provider,
 	p.password = password
 
 	var params = &service.NitroParams{
-		Url:       "http://" + p.host + ":" + strconv.Itoa(p.hostport),
+		Url:       p.host + ":" + strconv.Itoa(p.hostport),
 		Username:  p.username,
 		Password:  p.password,
 		SslVerify: p.validatecerts,
