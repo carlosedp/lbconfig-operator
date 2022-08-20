5d4288c7cdfbfc15b4317d6fa15645c3040bb1ae
Carlos de Paula <me@carlosedp.com>
2022-08-20 16:04:46 -0300

    Additional tests for backend controller

--- controllers/backend/backend_controller/backend_controller_test.go
+++ controllers/backend/backend_controller/backend_controller_test.go
@@ -26,6 +26,7 @@ package controller_test
 
 import (
 	"context"
+	"reflect"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -43,10 +44,70 @@ func TestBackendController(t *testing.T) {
 	RunSpecs(t, "Backend Controller Suite")
 }
 
+var loadBalancer = &lbv1.ExternalLoadBalancer{
+	ObjectMeta: metav1.ObjectMeta{
+		Name:      "dummy-backend",
+		Namespace: "default",
+	},
+	Spec: lbv1.ExternalLoadBalancerSpec{
+		Vip: "10.0.0.1",
+		Provider: lbv1.Provider{
+			Vendor: "Dummy",
+			Host:   "1.2.3.4",
+			Port:   443,
+			Creds:  "secretname",
+		},
+	},
+}
+
+var monitor = lbv1.Monitor{
+	Path:        "/",
+	Port:        80,
+	MonitorType: "http",
+}
+
+var pool = &lbv1.Pool{
+	Name: "test-pool",
+	Members: []lbv1.PoolMember{{
+		Node: lbv1.Node{
+			Name:   "test-node-1",
+			Host:   "1.1.1.1",
+			Labels: map[string]string{"node-role.kubernetes.io/master": ""},
+		},
+		Port: 80},
+		{
+			Node: lbv1.Node{
+				Name:   "test-node-2",
+				Host:   "1.1.1.2",
+				Labels: map[string]string{"node-role.kubernetes.io/master": ""},
+			},
+			Port: 80},
+	},
+}
+
+var poolmember = &lbv1.PoolMember{
+	Node: lbv1.Node{
+		Name:   "test-node-5",
+		Host:   "1.1.1.5",
+		Labels: map[string]string{"node-role.kubernetes.io/master": ""},
+	},
+	Port: 80,
+}
+
+var VIP = &lbv1.VIP{
+	Name: "test-vip",
+	Pool: pool.Name,
+	IP:   "1.2.3.4",
+}
+
 var _ = Describe("Controllers/Backend/controller/backend_controller", func() {
 
 	Context("When using a creating backends", func() {
 		var ctx = context.TODO()
+
+		It("Should validate that backend provider is registered", func() {
+			Expect(ListProviders()).Should(ContainElement("dummy"))
+		})
 
 		It("Should return error if backend provider tries to register again", func() {
 			err := RegisterProvider("Dummy", new(d.DummyProvider))
@@ -75,6 +136,42 @@ var _ = Describe("Controllers/Backend/controller/backend_controller", func() {
 			Expect(createdBackend).To(BeNil())
 		})
 
+		It("Should create a provider with registered backend provider", func() {
+			createdBackend, err := CreateBackend(ctx, &loadBalancer.Spec.Provider, "username", "password")
+			Expect(err).Should(BeNil())
+			Expect(reflect.TypeOf(createdBackend.Provider)).Should(Equal(reflect.TypeOf(&d.DummyProvider{})))
+		})
+
+		It("Should handle a provider monitor", func() {
+			createdBackend, err := CreateBackend(ctx, &loadBalancer.Spec.Provider, "username", "password")
+			Expect(err).Should(BeNil())
+			err = createdBackend.HandleMonitors(ctx, &monitor)
+			Expect(err).Should(BeNil())
+		})
+
+		It("Should handle a provider pool", func() {
+			createdBackend, err := CreateBackend(ctx, &loadBalancer.Spec.Provider, "username", "password")
+			Expect(err).Should(BeNil())
+			err = createdBackend.HandlePool(ctx, pool, &monitor)
+			Expect(err).Should(BeNil())
+		})
+
+		It("Should handle a provider VIP", func() {
+			createdBackend, err := CreateBackend(ctx, &loadBalancer.Spec.Provider, "username", "password")
+			Expect(err).Should(BeNil())
+			err = createdBackend.HandleVIP(ctx, VIP)
+			Expect(err).Should(BeNil())
+		})
+
+		It("Should handle a provider cleanup", func() {
+			createdBackend, err := CreateBackend(ctx, &loadBalancer.Spec.Provider, "username", "password")
+			Expect(err).Should(BeNil())
+			err = createdBackend.HandleCleanup(ctx, loadBalancer)
+			Expect(err).Should(BeNil())
+		})
+
+	})
+	Context("When using auxiliary functions", func() {
 		It("Should return true if array contains member", func() {
 			m := lbv1.PoolMember{
 				Node: lbv1.Node{
